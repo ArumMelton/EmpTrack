f403e076aad7b4763bb61d553de0a6a2456843c5
ArumMelton <103224098+ArumMelton@users.noreply.github.com>
2022-09-15 16:35:33 -0400

    updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,28 +1,28 @@
-INSERT INTO department (name)
- VALUES
+INSERT INTO department (department_name)
+VALUES
 ('Sales'),
 ('Engineering'),
-('Finance'),
-('Legal');
+('Product'),
+('Design');
 
-INSERT INTO roles (title, salary, department_id)
+INSERT INTO role (title, salary, department_id)
 VALUES
-("Sales Lead", 100000, 1),
-("Salesperson", 80000, 1),
-("Lead Engineer", 150000, 2),
-("Software Engineer", 120000, 2),
-("Account Manager", 160000, 3), 
-("Accountant", 125000, 3),
-("Legal Team Lead", 250000, 4),
-("Lawyer", 190000, 4);
+('Sales Lead', 100000, 1),
+('Salesperson', 80000, 1),
+('Lead Engineer', 150000, 2),
+('Software Engineer', 120000, 2),
+('Director of Product Management', 160000, 3),
+('Product Manager', 125000, 3),
+('Director of Design', 150000, 4),
+('UX/UI Designer', 120000, 4);
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-("John", "Doe", 1, NULL),
-("Mike", "Chan", 2, 1),
-("Ashley", "Rodriguez", 3, NULL),
-("Kevin", "Tupik", 4, 3),
-("Kunal", "Singh", 5, NULL), 
-("Malia", "Brown", 6, 5),
-("Sarah", "Lourd", 7, NULL),
-("Tom", "Allen", 8, 7);
+('Carolyn', 'Doe', 1, NULL),
+('Trey', 'Allen', 2, 1),
+('Josh', 'Highmen', 3, NULL),
+('Alex', 'Bruce', 4, 3),
+('Christine', 'Brown', 5, NULL),
+('John', 'Banks', 6, 5),
+('Jess', 'Burke', 7, NULL),
+('Janice', 'Allen', 8, 7);
